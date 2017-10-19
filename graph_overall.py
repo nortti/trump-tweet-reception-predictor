3e331f689c0322e6e38105f40d43481768127e03
tringuyenminh23 <tringuyenminh23@gmail.com>
2017-10-19 20:26:42 +0300

    finish overall graph; add retweet and favorite count seprate graph, along with html file

---
+++ graph_overall.py
@@ -0,0 +1,57 @@
+import plotly
+import plotly.graph_objs as go
+import random
+import numpy as np
+import pandas as pd
+
+# Credential
+plotly.tools.set_credentials_file(username='tringuyenminh23', api_key='Ujw0Z0wUWFUDAXiKEsZO')
+
+
+# Process data
+data = pd.read_json("json_data/realDonaldTrump.json")
+
+# Calculate accumulate respond 
+accumulate_responds = []
+for i in range(len(data)):
+    accumulate_responds.append(data.iloc[i]['retweet_count'] + data.iloc[i]['favorite_count'])
+
+# Normalize accumulate_responds to use as size of bubble
+accumulate_responds_normalized = [float(i)/max(accumulate_responds) for i in accumulate_responds]    
+sizeref = 2*max(accumulate_responds_normalized)/(80**2)
+
+
+fig_data = []
+trace0= go.Scatter(
+    x= data['favorite_count'],
+    y= data['retweet_count'],
+    mode= 'markers',
+    marker= dict(
+        symbol = 'circle',
+        sizemode = 'area',
+        sizeref = sizeref,
+        size = accumulate_responds_normalized,
+        line= dict(width=1),
+        color= 'rgb(93, 164, 214)',
+        opacity= 0.8),
+    text= data['text'])
+fig_data.append(trace0);
+
+layout= go.Layout(
+    title= "Donald Trump's most recent tweets and respond count",
+    hovermode= 'closest',
+    xaxis= dict(
+        title= 'Retweet Count',
+        ticklen= 5,
+        zeroline= False,
+        gridwidth= 2,
+    ),
+    yaxis=dict(
+        title= 'Favorite Count',
+        ticklen= 5,
+        gridwidth= 2,
+    ),
+    showlegend= False
+)
+fig = go.Figure(data = fig_data, layout=layout)
+plotly.offline.plot(fig, filename = 'overall_graph')
--- graph_retweet.py
+++ graph_retweet.py
@@ -1,34 +1,51 @@
-import plotly
-import plotly.graph_objs as go
-import random
-import numpy as np
+import plotly as py
+import plotly.graph_objs as go 
 import pandas as pd
 
-# Credential
-plotly.tools.set_credentials_file(username='tringuyenminh23', api_key='Ujw0Z0wUWFUDAXiKEsZO')
+from datetime import datetime
 
 
-# Process data
 data = pd.read_json("json_data/realDonaldTrump.json")
-data.sort_values('retweet_count', ascending = 0, inplace = True)
 
-data_top10 = data.head(10)
-data_low10 = data.tail(10)
-frames = [data_top10, data_low10]
-data_extracted = pd.concat(frames)
+trace = go.Scatter(
+    x = data.created_at, 
+    y = data.retweet_count,
+    text = data.text,)
 
-print(data_extracted)
+fig_data = [trace]
+layout = dict(
+    title="Tweets' retweet count with slider",
+    hovermode= 'closest',
+    xaxis=dict(
+    	title = "Time",
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1,
+                     label='1m',
+                     step='month',
+                     stepmode='backward'),
+                dict(count=6,
+                     label='6m',
+                     step='month',
+                     stepmode='backward'),
+                dict(count=1,
+                    label='YTD',
+                    step='year',
+                    stepmode='todate'),
+                dict(count=1,
+                    label='1y',
+                    step='year',
+                    stepmode='backward'),
+                dict(step='all')
+            ])
+        ),
+        rangeslider=dict(),
+        type='date'
+    ),
+    yaxis=dict(
+    	title = "Retweet Count",
+    )
+)
 
-# # trace0 = go.Scatter(
-# #     x= ,
-# #     y=[10, 11, 12, 13],
-# #     text=['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
-# #     mode='markers',
-# #     marker=dict(
-# #         color=['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-# #         size=[40, 60, 80, 100],
-# #     )
-# # )
-
-# data = [trace0]
-# py.iplot(data, filename='bubblechart-text')
+fig = dict(data=fig_data, layout=layout)
+py.offline.plot(fig, filename = 'retweet_graph')
